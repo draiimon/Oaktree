b0a2c69cd540b6cd8afa2a3f1e9f151d0320a15d
Mark Andrei Castillo <99703880+draiimon@users.noreply.github.com>
2025-03-13 01:03:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "HELLO OAKTREE INNOVATIONS"
+    return "HELLO OAKTREE INNOVATIONS MWA MWA"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
