7ce18b03bef501dd04e7da5eef8f3421c0a2d1d8
draiimon <mmacastillo@tip.edu.ph>
2025-03-21 07:51:38 +0800

    CI/CD pipeline for local testing na!

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log('Server running');
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('TEST IS RUNNING!');
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
